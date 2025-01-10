8847f06d3f7921eda24a55fa201e691565cb80da
MariaPoala <maria.vilcapoma22@gmail.com>
2025-01-10 12:08:51 -0500

    mejoras total

--- src/app/common/how-it-works/how-it-works.component.ts
+++ src/app/common/how-it-works/how-it-works.component.ts
@@ -5,7 +5,7 @@ import { RouterLink } from '@angular/router';
 @Component({
     selector: 'app-how-it-works',
     standalone: true,
-    imports: [RouterLink, NgFor,  NgIf],
+    imports: [RouterLink, NgFor, NgIf],
     templateUrl: './how-it-works.component.html',
     styleUrl: './how-it-works.component.scss'
 })
@@ -14,43 +14,72 @@ export class HowItWorksComponent {
     sectionTitle = [
         {
             subTitle: "Como empresa",
-            title: 'Eventos importantes',
+            title: 'Nuestros Valores',
             paragraphText: 'Conoce los hitos que han impulsado nuestro crecimiento y reafirmado nuestra posición como líderes en el sector.'
         }
     ]
     singleProcessBox = [
 
+        // {
+        //     img: 'images/process/img1.png',
+        //     title: 'Fundación de Constrarna',
+        //     paragraphText: 'Inicio de operaciones como una constructora comprometida con la excelencia y la innovación en proyectos de construcción.',
+        //     number: '1'
+        // },
+        // {
+        //     img: 'images/process/img2.png',
+        //     title: 'Primer Proyecto Emblemático Completado',
+        //     paragraphText: 'Culminación exitosa de un proyecto de gran envergadura que marcó un hito en nuestra historia.',
+        //     number: '2'
+        // },
+        // {
+        //     img: 'images/process/img3.png',
+        //     title: 'Expansión a Nuevas Regiones',
+        //     paragraphText: 'Apertura de operaciones en nuevas ciudades o regiones, llevando nuestros servicios a más comunidades.',
+        //     number: '3'
+        // },
+        // {
+        //     img: 'images/process/img4.png',
+        //     title: 'Reconocimiento a la Excelencia en Construcción',
+        //     paragraphText: 'Premio otorgado por entidades prestigiosas en reconocimiento a la calidad e innovación de nuestros proyectos.',
+        //     number: '4'
+        // },
+        // {
+        //     img: 'images/process/img5.png',
+        //     title: 'Inauguración de un Proyecto Insignia',
+        //     paragraphText: 'Apertura de un complejo residencial que resalta nuestro compromiso con el diseño y la funcionalidad.',
+        //     number: '5' 225 195
+        // }
         {
-            img: 'images/process/img1.png',
-            title: 'Fundación de Constrarna',
-            paragraphText: 'Inicio de operaciones como una constructora comprometida con la excelencia y la innovación en proyectos de construcción.',
+            title: 'Compromiso',
+            paragraphText: 'Nos dedicamos a cumplir con cada proyecto en tiempo y forma, priorizando la satisfacción de nuestros clientes y el respeto por los acuerdos establecidos.',
+            img: 'images/valores/Compromiso.jpg',
             number: '1'
         },
         {
-            img: 'images/process/img2.png',
-            title: 'Primer Proyecto Emblemático Completado',
-            paragraphText: 'Culminación exitosa de un proyecto de gran envergadura que marcó un hito en nuestra historia.',
-            number: '2'
-        },
-        {
-            img: 'images/process/img3.png',
-            title: 'Expansión a Nuevas Regiones',
-            paragraphText: 'Apertura de operaciones en nuevas ciudades o regiones, llevando nuestros servicios a más comunidades.',
+            title: 'Innovación',
+            paragraphText: 'Implementamos tecnologías avanzadas y técnicas modernas para ofrecer soluciones eficientes y sostenibles en cada proyecto.',
+            img: 'images/valores/Innovación.jpg',
             number: '3'
         },
         {
-            img: 'images/process/img4.png',
-            title: 'Reconocimiento a la Excelencia en Construcción',
-            paragraphText: 'Premio otorgado por entidades prestigiosas en reconocimiento a la calidad e innovación de nuestros proyectos.',
-            number: '4'
+            title: 'Calidad',
+            paragraphText: 'Trabajamos con los mejores materiales, herramientas y estándares, garantizando resultados duraderos y de excelencia en cada obra.',
+            img: 'images/valores/Calidad.png',
+            number: '2'
         },
         {
-            img: 'images/process/img5.png',
-            title: 'Inauguración de un Proyecto Insignia',
-            paragraphText: 'Apertura de un complejo residencial que resalta nuestro compromiso con el diseño y la funcionalidad.',
+            title: 'Confianza',
+            paragraphText: 'Construimos relaciones basadas en la transparencia y la comunicación, asegurando un trato honesto y profesional en todo momento.',
+            img: 'images/valores/confianza.jpg',
             number: '5'
+        },   
+        {
+            title: 'Responsabilidad',
+            paragraphText: 'Mantenemos un firme compromiso con la seguridad, el medio ambiente y el bienestar de todos los involucrados en nuestros proyectos.',
+            img: 'images/valores/Responsabilidad.jpg',
+            number: '4'
         }
-
 
     ]
 
--- src/app/common/testimonials/testimonials.component.ts
+++ src/app/common/testimonials/testimonials.component.ts
@@ -18,13 +18,13 @@ export class TestimonialsComponent {
         {
             titulo: 'Visión',
             paragraphText: 'Ser reconocidos como la constructora líder en el mercado nacional, destacándonos por ofrecer soluciones innovadoras y sostenibles en el sector de la construcción, contribuyendo al desarrollo urbano, industrial y residencial, mientras fomentamos un impacto positivo en la sociedad y el medio ambiente.',
-            clientImg: 'images/testimonials/img1.jpg',
+            clientImg: 'images/testimonials/img4.jpg',
 
         },
         {
             titulo: 'Misión',
             paragraphText: 'Proveer servicios de construcción de alta calidad, integrando tecnología, diseño y recursos sostenibles, para satisfacer las necesidades de nuestros clientes. Nos enfocamos en crear espacios funcionales y duraderos, con un compromiso constante hacia la excelencia, la seguridad y el respeto por el entorno.',
-            clientImg: 'images/testimonials/img2.jpg',
+            clientImg: 'images/testimonials/img4.jpg',
 
         }
     ]
